d6d9579a61e5b05490a0b7d19d6488b19e6d23b8
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-10-13 18:31:31 +0700

    feat(nvim): dbui preconnection

--- .nvim.lua
+++ .nvim.lua
@@ -10,6 +10,16 @@ vim.lsp.config("gopls", {
 
 vim.filetype.add({
   pattern = {
-    [".*%.sql"] = "pgsql"
-  }
+    [".*%.sql"] = "pgsql",
+  },
 })
+
+vim.env.DBUI_URL = string.format(
+  "postgres://%s:%s@%s:%s/%s?sslmode=disable",
+  vim.env.DB_USERNAME,
+  vim.env.DB_PASSWORD,
+  vim.env.DB_HOST,
+  vim.env.DB_PORT,
+  vim.env.DB_DATABASE
+)
+vim.env.DBUI_NAME = "electricilies"
