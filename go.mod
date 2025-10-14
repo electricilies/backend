8be9f779e125544e0e9a2fd03e3de032d0ccac06
NTGNguyen <ntgnguyen@duck.com>
2025-10-14 20:44:50 +0700

    feat: jwt and keycloak

--- go.mod
+++ go.mod
@@ -21,6 +21,7 @@ require (
 
 require (
 	github.com/KyleBanks/depth v1.2.1 // indirect
+	github.com/Nerzal/gocloak/v13 v13.9.0 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/bytedance/gopkg v0.1.3 // indirect
 	github.com/bytedance/sonic v1.14.1 // indirect
@@ -49,9 +50,11 @@ require (
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.27.0 // indirect
+	github.com/go-resty/resty/v2 v2.7.0 // indirect
 	github.com/go-viper/mapstructure/v2 v2.4.0 // indirect
 	github.com/goccy/go-json v0.10.5 // indirect
 	github.com/goccy/go-yaml v1.18.0 // indirect
+	github.com/golang-jwt/jwt/v5 v5.0.0 // indirect
 	github.com/google/subcommands v1.2.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
@@ -65,7 +68,9 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/opentracing/opentracing-go v1.2.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.4 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/prometheus/client_model v0.6.2 // indirect
 	github.com/prometheus/common v0.66.1 // indirect
@@ -74,6 +79,7 @@ require (
 	github.com/quic-go/quic-go v0.54.0 // indirect
 	github.com/russross/blackfriday/v2 v2.0.1 // indirect
 	github.com/sagikazarmark/locafero v0.11.0 // indirect
+	github.com/segmentio/ksuid v1.0.4 // indirect
 	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
 	github.com/sourcegraph/conc v0.3.1-0.20240121214520-5f936abd7ae8 // indirect
 	github.com/spf13/afero v1.15.0 // indirect
---
+++ internal/interface/api/middleware/jwt.go
@@ -0,0 +1,95 @@
+package middleware
+
+import (
+	"net/http"
+	"reflect"
+	"strings"
+
+	"github.com/Nerzal/gocloak/v13"
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type Auth interface {
+	Handler() gin.HandlerFunc
+}
+
+type auth struct {
+	keycloakClient *gocloak.GoCloak
+	keycloakHost   string
+	clientId       string
+	clientSecret   string
+	realm          string
+}
+
+func (j *auth) Handler() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Missing Authorization header"})
+			return
+		}
+		parts := strings.SplitN(authHeader, " ", 2)
+		if len(parts) != 2 || strings.ToLower(parts[0]) != "bearer" {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Invalid Authorization header format"})
+			return
+		}
+		token := parts[1]
+		rptResult, err := j.keycloakClient.RetrospectToken(c, token, j.clientId, j.clientSecret, j.realm)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Failed to introspect token", "detail": err.Error()})
+			return
+		}
+		if rptResult == nil || !*rptResult.Active {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Inactive or invalid token"})
+			return
+		}
+		tokens, _, err := j.keycloakClient.DecodeAccessToken(c, token, j.realm)
+
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Cannot decode access token", "detail": err.Error()})
+			return
+		}
+
+		claims, _ := tokens.Claims.(jwt.MapClaims)
+		sub := claims["sub"].(string)
+
+		roles := extractRoles(claims, j.clientId)
+		requiredRoles, err := j.keycloakClient.GetClientRolesByUserID(c, token, j.realm, j.clientId, sub)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "false to get role", "detail": err.Error()})
+		}
+		requiredRoleNames := make([]string, len(requiredRoles))
+		for i, u := range requiredRoles {
+			requiredRoleNames[i] = *u.Name
+		}
+		if equal := reflect.DeepEqual(roles, requiredRoleNames); !equal {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "you don't have enough roles"})
+			return
+
+		}
+	}
+}
+
+func extractRoles(claims jwt.MapClaims, clientID string) []string {
+	var roles []string
+
+	resAccess, _ := claims["resource_access"].(map[string]interface{})
+	if resAccess == nil {
+		return roles
+	}
+
+	clientRes, _ := resAccess[clientID].(map[string]interface{})
+	if clientRes == nil {
+		return roles
+	}
+
+	resRoles, _ := clientRes["roles"].([]interface{})
+	for _, r := range resRoles {
+		if role, _ := r.(string); role != "" {
+			roles = append(roles, role)
+		}
+	}
+
+	return roles
+}
