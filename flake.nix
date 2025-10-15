574197b1303bee36705ac0bdcf3ed701e19481a5
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-10-15 18:04:45 +0700

    chore(flake): adjust a bit

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-  outputs = {self, ...} @ inputs: let
+  outputs = {nixpkgs, ...}: let
     supportedSystems = [
       "x86_64-linux"
       "aarch64-linux"
@@ -9,9 +9,9 @@
       "aarch64-darwin"
     ];
     forEachSupportedSystem = f:
-      inputs.nixpkgs.lib.genAttrs supportedSystems (system:
+      nixpkgs.lib.genAttrs supportedSystems (system:
         f {
-          pkgs = import inputs.nixpkgs {
+          pkgs = import nixpkgs {
             inherit system;
           };
         });
