e700ea2049f255ea0715faf6177e0f16b5ce885c
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-10-17 01:30:01 +0700

    chore: no swaggo_ls

--- .nvim.lua
+++ .nvim.lua
@@ -8,9 +8,9 @@ vim.lsp.config("gopls", {
   },
 })
 
-if vim.lsp.config["swaggo_ls"] then
-  vim.lsp.enable("swaggo_ls")
-end
+-- if vim.lsp.config["swaggo_ls"] then
+--  vim.lsp.enable("swaggo_ls")
+-- end
 
 vim.filetype.add({
   pattern = {
