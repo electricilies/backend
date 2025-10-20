2561f95441fbf091115228279ed06a872ef4595b
NTGNguyen <ntgnguyen@duck.com>
2025-10-20 09:17:42 +0700

    feat(nvim): switching build flag

--- .nvim.lua
+++ .nvim.lua
@@ -1,3 +1,5 @@
+local map = vim.keymap.set
+
 vim.lsp.config("gopls", {
   settings = {
     gopls = {
@@ -7,6 +9,30 @@ vim.lsp.config("gopls", {
     },
   },
 })
+
+map("n", "<localleader>lb", function()
+  vim.ui.select({
+    "none",
+    "integration",
+    "wireinject",
+  }, {
+    prompt = "Select gopls build flags:",
+  }, function(flag)
+    vim.lsp.config["gopls"] = {
+      settings = {
+        buildFlags = {
+          "-tags" .. flag,
+        },
+      },
+    }
+    for _, client in ipairs(vim.lsp.client()) do
+      if client.name == "gopls" then
+        vim.lsp.stop_client(client, true)
+      end
+    end
+    vim.lsp.start(vim.lsp.config["gopls"])
+  end)
+end, { desc = "LSP | Switch buildFlags", silent = true })
 
 -- if vim.lsp.config["swaggo_ls"] then
 --  vim.lsp.enable("swaggo_ls")
