8d17cbd5f0a52391aa82b00dea802c37274a392e
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-11-03 21:30:10 +0700

    fix(database): rating not rate

--- database/schema.sql
+++ database/schema.sql
@@ -72,7 +72,7 @@ CREATE TABLE product_images (
 -- reviews
 CREATE TABLE reviews (
   id SERIAL PRIMARY KEY,
-  rating INTEGER NOT NULL CHECK (rate >= 1 AND rate <= 5),
+  rating INTEGER NOT NULL CHECK (rating >= 1 AND rating <= 5),
   content TEXT,
   image_url TEXT NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT NOW(),
