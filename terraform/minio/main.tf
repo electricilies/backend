21e973c37ad1b9e94e6433709dde6a63cacaa862
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-11-09 00:12:13 +0700

    chore(minio): use policy to cleanup

--- terraform/minio/main.tf
+++ terraform/minio/main.tf
@@ -8,25 +8,25 @@ terraform {
   }
 }
 
-resource "minio_config" "notify_webhook_backend" {
-  key   = "notify_webhook:backend"
-  value = "endpoint=${var.backend_webhook} auth_token= queue_limit=0 queue_dir= client_cert= client_key="
-}
-
 resource "minio_s3_bucket" "electricilies" {
   acl    = "public"
   bucket = "electricilies"
 }
 
-resource "minio_s3_bucket_notification" "backend" {
+resource "minio_ilm_policy" "electricilies_policy" {
   bucket = minio_s3_bucket.electricilies.bucket
 
-  queue {
-    id        = "notification-webook-backend"
-    queue_arn = "arn:minio:sqs::backend:webhook"
-    events = [
-      "s3:ObjectCreated:*",
-      "s3:ObjectRemoved:*",
-    ]
+  rule {
+    id         = "expire-temp-product-images"
+    status     = "Enabled"
+    filter     = "temp/products/images/"
+    expiration = "1d"
+  }
+
+  rule {
+    id         = "expire-temp-reviews"
+    status     = "Enabled"
+    filter     = "temp/reviews/images/"
+    expiration = "1d"
   }
 }
